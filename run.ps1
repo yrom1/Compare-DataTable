c49ad0bda68d80448509004c399d2d7a4ca42abd
Ryan Moore <ryanm.inbox@gmail.com>
2023-01-10 21:06:49 -0500

    add try/catch to run cuz

--- run.ps1
+++ run.ps1
@@ -1,8 +1,20 @@
 $pc = $env:COMPUTERNAME
 $ver = (Get-ComputerInfo).WindowsProductName
-Write-Output "You are the computer {0} running {1}" -f $pc, $ver
-Write-Output "Running Test-DataSet"
+$hi = "You are the computer {0} running {1}" -f $pc, $ver
+Write-host $hi
 
+try {
+    $ErrorActionPreference = "Stop"
+    Write-Output "Testing Test-DataSet"
+    .\test.ps1
+}
+catch {
+    # $ex = $_
+    Write-Output "Test Failure!"
+    throw
+}
+Write-Output "Test Success!"
+
+Write-Output "Running Test-DataSet Example"
 Import-Module .\Test-DataSet
-
 Test-DataSet -p0 'a' -p1 'b'
